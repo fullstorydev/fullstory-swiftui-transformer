8db59cde76202fde61093b9f60a0cb20ab1cec66
Martin Hock <martin@fullstory.com>
2024-02-21 13:42:20 -0800

    Test example: copy files

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "fullstory-swiftui-plugin",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
+        .plugin(
             name: "fullstory-swiftui-plugin",
-            targets: ["fullstory-swiftui-plugin"]),
+            targets: ["FullStoryBuildPlugin"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "fullstory-swiftui-plugin",
+        .plugin(
+            name: "FullStoryBuildPlugin",
+            capability: .buildTool(),
             dependencies: []),
-        .testTarget(
-            name: "fullstory-swiftui-pluginTests",
-            dependencies: ["fullstory-swiftui-plugin"]),
     ]
 )
---
+++ Plugins/FullStoryBuildPlugin/FullStoryBuildPlugin.swift
@@ -0,0 +1,24 @@
+import Foundation
+import PackagePlugin
+
+@main
+struct FullStoryBuildPlugin: BuildToolPlugin {
+
+    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+        guard let target = target as? any SourceModuleTarget else { return [] }
+        let inputFiles = target.sourceFiles.filter({ $0.path.extension == "swift" })
+        return try inputFiles.map {
+            let inputFile = $0
+            let inputPath = inputFile.path
+            let outputName = inputPath.stem + "-copy.swift"
+            let outputPath = context.pluginWorkDirectory.appending(outputName)
+            return .buildCommand(
+                displayName: "Generating \(outputName) from \(inputPath.lastComponent)",
+                executable: try context.tool(named: "cp").path,
+                arguments: [ "\(inputPath)", "\(outputPath)" ],
+                inputFiles: [ inputPath, ],
+                outputFiles: [ outputPath ]
+            )
+        }
+    }
+}
