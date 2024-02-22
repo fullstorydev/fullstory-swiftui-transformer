f1119f5b38a607b8b485d612c30b7a1423071e84
Martin Hock <martin@fullstory.com>
2024-02-21 16:34:58 -0800

    Add XcodeBuildToolPlugin support

--- Plugins/FullStoryBuildPlugin/FullStoryBuildPlugin.swift
+++ Plugins/FullStoryBuildPlugin/FullStoryBuildPlugin.swift
@@ -22,3 +22,27 @@ struct FullStoryBuildPlugin: BuildToolPlugin {
         }
     }
 }
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension FullStoryBuildPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        guard let target = target as? any SourceModuleTarget else { return [] }
+        let inputFiles = target.sourceFiles.filter({ $0.path.extension == "swift" })
+        return try inputFiles.map {
+            let inputFile = $0
+            let inputPath = inputFile.path
+            let outputName = inputPath.stem + "-copy.swift"
+            let outputPath = context.pluginWorkDirectory.appending(outputName)
+            return .buildCommand(
+                displayName: "Generating \(outputName) from \(inputPath.lastComponent)",
+                executable: try context.tool(named: "cp").path,
+                arguments: [ "\(inputPath)", "\(outputPath)" ],
+                inputFiles: [ inputPath, ],
+                outputFiles: [ outputPath ]
+            )
+        }
+    }
+}
+#endif
