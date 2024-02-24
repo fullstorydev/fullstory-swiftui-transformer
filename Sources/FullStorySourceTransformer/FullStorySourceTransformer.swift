15883949c32b7c685a6973db146b24fa86780959
Martin Hock <martin@fullstory.com>
2024-02-23 16:58:09 -0800

    Transform and output source

--- Sources/FullStorySourceTransformer/FullStorySourceTransformer.swift
+++ Sources/FullStorySourceTransformer/FullStorySourceTransformer.swift
@@ -3,10 +3,13 @@ import Foundation
 @main
 struct FullStorySourceTransformer {
     static func main() throws {
-        NSLog("arguments: %@", CommandLine.arguments)
         if CommandLine.arguments.count == 3 {
-            let str = try String(contentsOfFile: CommandLine.arguments[1])
-            NSLog("file: %@", str)
+            var str = try String(contentsOfFile: CommandLine.arguments[1])
+            // TODO: allow whitespace with newlines; preserve newlines
+            str = str.replacing(#/import SwiftUI/#, with: "import SwiftUI; import FullStory")
+            str = str.replacing(#/var body: some View/#, with: "var originalBody: some View")
+            str = str.replacing(#/var body: some View/#, with: "var body: some View { originalBody.fsSelectable() }; var originalBody: some View")
+            try str.write(toFile: CommandLine.arguments[2], atomically: true, encoding: String.Encoding.utf8)
         }
     }
 }
